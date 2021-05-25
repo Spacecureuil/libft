bc9d44a7c3c30c2ba0760ca68ca5fee5e1d7bee3
Spacecureuil <p.maury.skj@gmail.com>
2021-05-25 17:34:50 +0200

    cat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
